3e5e1e3618565c8fa7050e16b721046609c6b4eb
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-08-13 09:04:52 +0530

    Config phaser

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('hello')
+let gameScene = new Phaser.Scene('Game');
+
+gameScene.init = function () { };
+
+gameScene.create = function () { };
+
+let config = {
+  type: Phaser.AUTO,
+  width: 360,
+  height: 640,
+  scene: gameScene,
+  title: 'Monster Kong',
+  pixelArt: false
+};
+
+let game = new Phaser.Game(config);
