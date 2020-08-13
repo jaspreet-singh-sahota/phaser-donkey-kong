fecc2821857a1826ef465800490b50bf8d1880ae
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-08-14 03:03:30 +0530

    Refactor code

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 import './styles/style.css';
 
+// create a new scene
 let gameScene = new Phaser.Scene('Game');
 
+// some parameters for our scene
 gameScene.init = function () {
+
+  // player parameters
   this.playerSpeed = 150;
   this.jumpSpeed = -600;
 };
 
+// load asset files for our game
 gameScene.preload = function () {
+
+  // load images
   this.load.image('ground', 'assets/images/ground.png');
   this.load.image('platform', 'assets/images/platform.png');
   this.load.image('block', 'assets/images/block.png');
   this.load.image('goal', 'assets/images/gorilla3.png');
   this.load.image('barrel', 'assets/images/barrel.png');
 
+  // load spritesheets
   this.load.spritesheet('player', 'assets/images/player_spritesheet.png', {
     frameWidth: 28,
     frameHeight: 30,
@@ -31,67 +39,45 @@ gameScene.preload = function () {
   this.load.json('levelData', 'assets/json/levelData.json');
 };
 
+// executed once, after assets were loaded
 gameScene.create = function () {
-  
-  // world bounds
-  this.physics.world.bounds.width = 360;
-  this.physics.world.bounds.height = 700;
-  
-  this.platforms = this.add.group();
-  // // ground
-  // let ground = this.add.sprite(180, 604, 'ground');
-  // this.physics.add.existing(ground, true);
-  // this.platforms.add(ground);
 
-  // // platform
-  // let platform = this.add.tileSprite(180, 500, 4 * 36, 1 * 30, 'block');
-  // this.physics.add.existing(platform, true);
-  // this.platforms.add(platform);
+  if (!this.anims.get('walking')) {
+    // walking animation
+    this.anims.create({
+      key: 'walking',
+      frames: this.anims.generateFrameNames('player', {
+        frames: [0, 1, 2]
+      }),
+      frameRate: 12,
+      yoyo: true,
+      repeat: -1
+    });
+  }
+
+  if (!this.anims.get('burning')) {
+    // fire animation
+    this.anims.create({
+      key: 'burning',
+      frames: this.anims.generateFrameNames('fire', {
+        frames: [0, 1]
+      }),
+      frameRate: 4,
+      repeat: -1
+    });
+  }
 
   // add all level elements
   this.setupLevel();
 
-  // walking animation
-  this.anims.create({
-    key: 'walking',
-    frames: this.anims.generateFrameNames('player', {
-      frames: [0, 1, 2]
-    }),
-    frameRate: 12,
-    yoyo: true,
-    repeat: -1
-  });
-
-  // fire animation
-  this.anims.create({
-    key: 'burning',
-    frames: this.anims.generateFrameNames('fire', {
-      frames: [0, 1]
-    }),
-    frameRate: 4,
-    repeat: -1
-  });
-
-  // // player
-  // this.player = this.add.sprite(180, 400, 'player', 3);
-  // this.physics.add.existing(this.player);
-
-  // constrain player to the game bounds
-  this.player.body.setCollideWorldBounds(true);
-
-  // disable gravity
-  //ground.body.allowGravity = false;
-
-  // make it immovable
-  //ground.body.immovable = true;
-
-  // 2) creating and adding sprites to the physics system
-  //let ground2 = this.physics.add.sprite(180, 200, 'ground');
+  // initiate barrel spawner
+  this.setupSpawner();
 
   // collision detection
-  //this.physics.add.collider(ground, ground2);
-  this.physics.add.collider(this.player, this.platforms);
-  this.physics.add.collider(this.goal, this.platforms);
+  this.physics.add.collider([this.player, this.goal, this.barrels], this.platforms);
+
+  // overlap checks
+  this.physics.add.overlap(this.player, [this.fires, this.goal, this.barrels], this.restartGame, null, this);
 
   // enable cursor keys
   this.cursors = this.input.keyboard.createCursorKeys();
@@ -103,8 +89,8 @@ gameScene.create = function () {
 
 // executed on every frame
 gameScene.update = function () {
-  let onGround = this.player.body.blocked.down ||
-    this.player.body.touching.down;
+  // are we on the ground?
+  let onGround = this.player.body.blocked.down || this.player.body.touching.down;
 
   // movement to the left
   if (this.cursors.left.isDown) {
@@ -126,8 +112,7 @@ gameScene.update = function () {
     // play animation if none is playing
     if (onGround && !this.player.anims.isPlaying)
       this.player.anims.play('walking');
-  }
-  else {
+  } else {
     // make the player stop
     this.player.body.setVelocityX(0);
 
@@ -154,12 +139,16 @@ gameScene.update = function () {
 
 // sets up all the elements in the level
 gameScene.setupLevel = function () {
-  this.platforms = this.add.group();
 
-  // parse json data
+  // load json data
   this.levelData = this.cache.json.get('levelData');
 
+  // world bounds
+  this.physics.world.bounds.width = this.levelData.world.width;
+  this.physics.world.bounds.height = this.levelData.world.height;
+
   // create all the platforms
+  this.platforms = this.physics.add.staticGroup();
   for (let i = 0; i < this.levelData.platforms.length; i++) {
     let curr = this.levelData.platforms[i];
 
@@ -185,16 +174,14 @@ gameScene.setupLevel = function () {
   }
 
   // create all the fire
-  this.fires = this.add.group();
+  this.fires = this.physics.add.group({
+    allowGravity: false,
+    immovable: true
+  });
   for (let i = 0; i < this.levelData.fires.length; i++) {
     let curr = this.levelData.fires[i];
 
     let newObj = this.add.sprite(curr.x, curr.y, 'fire').setOrigin(0);
-
-    // enable physics
-    this.physics.add.existing(newObj);
-    newObj.body.allowGravity = false;
-    newObj.body.immovable = true;
 
     // play burning animation
     newObj.anims.play('burning');
@@ -223,12 +210,71 @@ gameScene.setupLevel = function () {
   // constraint player to the game bounds
   this.player.body.setCollideWorldBounds(true);
 
+  // camera bounds
+  this.cameras.main.setBounds(0, 0, this.levelData.world.width, this.levelData.world.height);
+  this.cameras.main.startFollow(this.player);
+
+
   // goal
   this.goal = this.add.sprite(this.levelData.goal.x, this.levelData.goal.y, 'goal');
   this.physics.add.existing(this.goal);
-
 };
 
+// restart game (game over + you won!)
+gameScene.restartGame = function (sourceSprite, targetSprite) {
+  // fade out
+  this.cameras.main.fade(500);
+
+  // when fade out completes, restart scene
+  this.cameras.main.on('camerafadeoutcomplete', function (camera, effect) {
+    // restart the scene
+    this.scene.restart();
+  }, this);
+};
+
+// generation of barrels
+gameScene.setupSpawner = function () {
+  // barrel group
+  this.barrels = this.physics.add.group({
+    bounceY: 0.1,
+    bounceX: 1,
+    collideWorldBounds: true
+  });
+
+  // spawn barrels
+  let spawningEvent = this.time.addEvent({
+    delay: this.levelData.spawner.interval,
+    loop: true,
+    callbackScope: this,
+    callback: function () {
+      // create a barrel
+      let barrel = this.barrels.get(this.goal.x, this.goal.y, 'barrel');
+
+      // reactivate
+      barrel.setActive(true);
+      barrel.setVisible(true);
+      barrel.body.enable = true;
+
+      // set properties
+      barrel.setVelocityX(this.levelData.spawner.speed);
+
+      //console.log(this.barrels.getChildren().length);
+
+      // lifespan
+      this.time.addEvent({
+        delay: this.levelData.spawner.lifespan,
+        repeat: 0,
+        callbackScope: this,
+        callback: function () {
+          this.barrels.killAndHide(barrel);
+          barrel.body.enable = false;
+        }
+      });
+    }
+  });
+};
+
+// our game's configuration
 let config = {
   type: Phaser.AUTO,
   width: 360,
@@ -239,10 +285,13 @@ let config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 1000 },
-      debug: true
+      gravity: {
+        y: 1000
+      },
+      debug: false
     }
   }
 };
 
+// create the game, and pass it the configuration
 let game = new Phaser.Game(config);
