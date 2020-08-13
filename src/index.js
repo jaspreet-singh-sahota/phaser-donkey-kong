1c6babc8dce948f72a07b93d6b1cba9a8655c9a8
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-08-13 09:06:19 +0530

    Add sprite sheet for plaer and fire

--- src/index.js
+++ src/index.js
@@ -9,7 +9,19 @@ gameScene.preload = function () {
   this.load.image('goal', 'assets/images/gorilla3.png');
   this.load.image('barrel', 'assets/images/barrel.png');
 
- 
+  this.load.spritesheet('player', 'assets/images/player_spritesheet.png', {
+    frameWidth: 28,
+    frameHeight: 30,
+    margin: 1,
+    spacing: 1
+  });
+
+  this.load.spritesheet('fire', 'assets/images/fire_spritesheet.png', {
+    frameWidth: 20,
+    frameHeight: 21,
+    margin: 1,
+    spacing: 1
+  });
 };
 
 gameScene.create = function () { };
