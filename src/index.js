dac8e9c34eff8f4128b6b8f676c9a91b93505b02
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-08-13 09:05:37 +0530

    Add images in pre loader function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ let gameScene = new Phaser.Scene('Game');
 
 gameScene.init = function () { };
 
+gameScene.preload = function () {
+  this.load.image('ground', 'assets/images/ground.png');
+  this.load.image('platform', 'assets/images/platform.png');
+  this.load.image('block', 'assets/images/block.png');
+  this.load.image('goal', 'assets/images/gorilla3.png');
+  this.load.image('barrel', 'assets/images/barrel.png');
+
+ 
+};
+
 gameScene.create = function () { };
 
 let config = {
